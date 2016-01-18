cbc994eb5fe03d866b19e8d909aa888daff95ebd
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-18 20:28:19 +0100

    add /m to build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild GitVersioner.sln  /property:Configuration=Release /property:Platform="Any CPU"
+%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild GitVersioner.sln /m /property:Configuration=Release /property:Platform="Any CPU"
 if exist bin\release\gitversioner.exe bin\release\gitversioner.exe ba
