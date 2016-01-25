d796be5d1ba54ea734d1065dcccd3b6f87331105
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-25 17:24:56 +0100

    add license

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Můžete zadat všechny hodnoty nebo nastavit výchozí očíslování sestavení a revize 
 // použitím znaku '*' jak je ukázáno dále:
-// [assembly: AssemblyVersion("1.0.1.6")]
-[assembly: AssemblyVersion("1.0.1.6")]
-[assembly: AssemblyFileVersion("1.0.1.6")]
+// [assembly: AssemblyVersion("1.0.1.8")]
+[assembly: AssemblyVersion("1.0.1.8")]
+[assembly: AssemblyFileVersion("1.0.1.8")]
