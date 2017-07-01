8423a1d0b09b486d988c1046e35636e4386390da
Michal Kuncl <michal.kuncl@gmail.com>
2017-07-01 10:13:33 +0200

    switch to .net 4.0 and add helpers class

---
+++ Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GitVersioner
+{
+    /// <summary>
+    ///     Class extension helpers
+    /// </summary>
+    public static class Helpers
+    {
+        /// <summary>
+        ///     Tries to convert a string to an int.
+        /// </summary>
+        /// <param name="stringNumber">a string</param>
+        /// <returns>zero if failed</returns>
+        public static int TryToInt32(this string stringNumber)
+        {
+            int result;
+            try
+            {
+                result = Convert.ToInt32(stringNumber);
+            }
+            catch
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
